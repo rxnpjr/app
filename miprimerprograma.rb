2d895556857a2338dacc3c3931c951ca576aa78e
Roxanna <rxnpjr@gmail.com>
2015-01-16 21:54:39 -0500

    Probando

--- miprimerprograma.rb
+++ miprimerprograma.rb
@@ -1,2 +1,44 @@
 puts "hola rxn"
 
+puts "hola mundo"
+# tengo_plata = true 
+# esta_lloviendo = false
+# if tengo_plata == true
+# 	puts "salgo" 
+# else esta_lloviendo == false
+# 	puts "no salgo"
+# end
+
+# a = "lapiz"
+# b = "cuaderno"
+# a = true
+# b = false 
+
+# if a && b
+
+# puts "puedo escribir"
+
+# else
+
+# puts "no puedo"
+
+# end 		
+
+a = "gato"
+b = "perro"
+c = "mico"
+a = true
+b = true
+c = false
+
+# if a || b
+# 	puts "gatoperro"
+# else 
+# 	puts "no sirvio"
+# end
+
+if c != a
+	puts "sirve"
+else
+	puts "no sirve"
+end
